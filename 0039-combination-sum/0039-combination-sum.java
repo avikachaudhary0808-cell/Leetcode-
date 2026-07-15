4f06eed2c47129fa48d1c7809a46b44aa9e3fb4b
avikachaudhary0808-cell <avikachaudhary0808@gmail.com>
2026-07-15 10:50:12 +0530

    Time: 3 ms (35.18%), Space: 46 MB (19.12%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution {
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        backtrack(candidates, target, 0,
+                  new ArrayList<>(), ans);
+
+        return ans;
+    }
+
+    private void backtrack(int[] candidates,
+                           int target,
+                           int index,
+                           List<Integer> current,
+                           List<List<Integer>> ans) {
+
+        if (target == 0) {
+
+            ans.add(new ArrayList<>(current));
+            return;
+        }
+
+        if (target < 0 || index == candidates.length)
+            return;
+
+        // Include current number
+        current.add(candidates[index]);
+
+        backtrack(candidates,
+                  target - candidates[index],
+                  index,
+                  current,
+                  ans);
+
+        current.remove(current.size() - 1);
+
+        // Exclude current number
+        backtrack(candidates,
+                  target,
+                  index + 1,
+                  current,
+                  ans);
+    }
+}
